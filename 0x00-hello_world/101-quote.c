d0b4d95053c45bf3e058cd1a263c4e5cf77ff20e
Salah <salah.elhssani@gmail.com>
2023-03-16 14:51:36 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, err, sizeof(err));
+	return (1);
+}
