5c772b640ecad5c2b193493de809c7c2f7c38e5e
Salah <salah.elhssani@gmail.com>
2023-03-16 15:08:47 -0700

    Fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, err, sizeof(err)-1);
+	write(2, err, sizeof(err) - 1);
 	return (1);
 }
