395733aaf34e765ad662be8406a92329234d1cc8
Salah <salah.elhssani@gmail.com>
2023-03-17 18:15:22 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,11 @@
  */
 int main(void)
 {
-	char alpha[] = "0123456789";
 	int i;
 
-	for (i = 0; i < 10 ; i++)
+	for (i = 48; i < 58 ; i++)
 	{
-		putchar(alpha[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
