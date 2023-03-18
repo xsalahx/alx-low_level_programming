d786f58c97c40f32444e1d0c773c28b88894a619
Salah <salah.elhssani@gmail.com>
2023-03-17 18:59:16 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i < 58 ; i++)
+	{
+		for (j = 48 ; j < 58 ; j++)
+		{
+			for (k = 48; k < 58 ; k++)
+			{
+				for (l = 48; l < 58 ; l++)
+				{
+					if (i * 10 + j < k * 10 + l)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(l);
+					
+						if (i != 57 || j != 56 || k != 57 || l != 57)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
