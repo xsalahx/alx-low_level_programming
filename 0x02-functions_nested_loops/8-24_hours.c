79c6d595ad59b36611aa2b9951e026b887c9ee72
Salah <salah.elhssani@gmail.com>
2023-03-21 15:22:30 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ */
+void jack_bauer(void)
+{
+	int h1, h2, m1, m2 = 0;
+
+	for (h1 = 0; h1 < 3; h1++)
+	{
+		for (h2 = 0; h2 < 10; h2++)
+		{
+			if (h1 == 2 && h2 == 5)
+			{
+				break;
+			}
+			for (m1 = 0; m1 < 6; m1++)
+			{
+				for (m2 = 0; m2 < 10; m2++)
+				{
+					_putchar('0' + h1);
+					_putchar('0' + h2);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
