0b80ce1754ba304a81f856059e1c1df9f9429451
Salah <salah.elhssani@gmail.com>
2023-03-21 14:38:35 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - Computes the absolute value of an integer
+ * @z: integer
+ *
+ * Return: Absolute value of z
+ */
+int _abs(int z)
+{
+	if (z < 0)
+	{
+		return (-z);
+	}
+	else
+	{
+		return (z);
+	}
+}
