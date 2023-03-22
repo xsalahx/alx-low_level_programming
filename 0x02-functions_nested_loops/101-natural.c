f209aa36145ab68aa21a752a3b24114472a65b0f
Salah <salah.elhssani@gmail.com>
2023-03-21 18:37:53 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
 
 /**
- * powr - calculate the power
- * @a: num1
- * @b: num2
- *
- * Return: a^b
- */
-int powr(int a, int b)
-{
-	int n = 1;
-	int i;
-
-	for (i = 0; i < b ; i++)
-	{
-		n = n * a;  
-	}
-	return (n);
-}
-
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j;
-	double n = 0;
+	int i;
+	int n = 0;
 
-	for (i = 1; i < 13; i++)
+	for (i = 1; i < 1024; i++)
 	{
-		for (j = 1; j < 12; j++)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			if (powr(3, i) * powr(5, i) < 1024)
-			{
-				n = n + powr(3, i) * powr(5, i);
-			}
+			n = n + i;
 		}
 	}
-	printf("%.0f\n", n);
+	printf("%d\n", n);
 	return (0);
 }
