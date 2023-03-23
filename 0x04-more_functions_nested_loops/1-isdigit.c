2593b8d9e8c1d9f95dc7b2ff19ce4a3ccc0f5f7d
Salah <salah.elhssani@gmail.com>
2023-03-23 14:01:20 -0700

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character (int)
+ *
+ * Return: 1 if uppercase 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
