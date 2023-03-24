62eb529cd1d51acb330a48b061885a942270a2a8
Salah <salah.elhssani@gmail.com>
2023-03-23 17:38:26 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,21 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number
- * @c: Number (int)
- *
- * Return: 1 if c > 0, 0 if c = 0 and -1 if c < 0.
- */
-int print_sign(int c)
-{
-	if (c < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else if (c == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		return (1);
-	}
-
-}
-
-/**
  * print_number - print number using _putchar
  * @n: Number
  */
 void print_number(int n)
 {
-	int p = 10;
-	int i;
-	int s = (n > 0) - (n < 0);
+	unsigned int i = n;
 
-	while (1)
+	if (n < 0)
 	{
-		if (n / p == 0)
-		{
-			break;
-		}
-		p = p * 10;
+		_putchar(45);
+		i = -i;
 	}
-	print_sign(n);
-	for (i = p; i >= 10; i = i / 10)
+	if (i / 10)
 	{
-		_putchar('0' + (s * ((n % i) / (i / 10))));
+		print_number(i / 10);
 	}
+	_putchar(i % 10 + '0');
 }
