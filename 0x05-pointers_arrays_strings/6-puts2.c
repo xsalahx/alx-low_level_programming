490c85de1374bd0783749ab60453b9dddd68a28e
Salah <salah.elhssani@gmail.com>
2023-03-26 08:22:05 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints odd charaters of string
+ * @*str: pointer to string
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		if (*str == '\0')
+		{
+			break;
+		}
+		str++;
+	}
+	_putchar('\n');
+}
