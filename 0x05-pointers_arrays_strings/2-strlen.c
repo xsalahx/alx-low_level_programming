c4ad3b69bf1c292d35ff6e3b8633f9ab47199c23
Salah <salah.elhssani@gmail.com>
2023-03-26 05:23:23 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/**
+ * _strlen - function that returns the length of a string
+ * @*s: pointer to a string (char)
+ *
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+	int strlen = 0;
+
+	while (*s != '\0')
+	{
+		strlen++;
+		s++;
+	}
+	return (strlen);
+}
