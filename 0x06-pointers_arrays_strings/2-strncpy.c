61d0645ad71fd0c0f9792bd3eb2ea151d122486a
Salah <salah.elhssani@gmail.com>
2023-03-29 14:48:35 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,16 +12,17 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	int stop = 0;
 
-	for  (i = 0; i <= n; i++)
+	for  (i = 0; i < n; i++)
 	{
 		if (*(src + i) == '\0' || stop == 1)
 		{
-			stop = 1;
+			
 			*(p + i) = '\0';
+			stop = 1;
 		}
 		else
 		{
-			if(i < n)
+			if (i < n)
 			{
 				*(p + i) = *(src + i);
 			}
