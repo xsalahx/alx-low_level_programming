15bb56522ca586ce16fa02a05b30d8d2c8e605ef
Salah <salah.elhssani@gmail.com>
2023-03-29 13:59:50 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+/**
+ * _strncpy - copies a string
+ * @dest: destination
+ * @src: source
+ * @n: max lenght
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *p = dest;
+	int i;
+	int stop = 0;
+
+	for  (i = 0; i <= n; i++)
+	{
+		if (*(src + i) == '\0' || stop == 1)
+		{
+			stop = 1;
+			*(p + i) = '\0';
+		}
+		else
+		{
+			*(p + i) = *(src + i);
+		}
+	}
+	return (dest);
+}
