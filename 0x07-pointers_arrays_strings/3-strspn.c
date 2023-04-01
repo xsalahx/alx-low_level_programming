e59cacb2803a51df53aece9d9e6ea30656526593
Salah <salah.elhssani@gmail.com>
2023-04-01 14:30:13 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string
+ * @accept: list of accepted caracters
+ *
+ * Return: number of bytes in the initial segment of s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int l = 0;
+	unsigned int c = 0;
+	int i = 0;
+	int j;
+
+	while (s[i])
+	{
+		j = 0;
+		while (accept[j])
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			else if (accept[j + 1] == 0)
+			{
+				l = (l >= c) * l + (l < c) * c;
+				c = 0;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (l);
+}
