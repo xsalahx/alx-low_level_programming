c83a938e5155b96b45196972b4b92af54dd2851f
Salah <salah.elhssani@gmail.com>
2023-04-05 03:50:30 -0700

    Fix task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,11 +6,14 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*(s + 1) == 0)
-		_putchar(*s);
-	else
+	if (*s)
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		if (*(s + 1) == 0)
+			_putchar(*s);
+		else
+		{
+			_print_rev_recursion(s + 1);
+			_putchar(*s);
+		}
 	}
 }
