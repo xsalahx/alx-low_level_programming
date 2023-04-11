465941d3fba82fb9b3eb91c5d0f7032f1045d322
Salah <salah.elhssani@gmail.com>
2023-04-11 03:27:44 -0700

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+/**
+ * _strdup - create copy of a string
+ * @str: string
+ *
+ * Return: string copy
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	while (str[i])
+	{
+		i++;
+	}
+	s = (char *) malloc ((i + 1) * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (i >= 0)
+		{
+			s[i] = str[i];
+			i--;
+		}
+	}
+	return (s);
+
+}
