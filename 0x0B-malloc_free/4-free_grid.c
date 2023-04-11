96c714109d54dc81213ce20714a93231a0118686
Salah <salah.elhssani@gmail.com>
2023-04-11 06:01:44 -0700

    Task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2 dimensional grid previously created
+ * @grid: grid
+ * @height : int
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid)
+	{
+		for (i = 0; i < height; i++)
+			free(grid[i]);
+		free(grid);
+	}
+}
