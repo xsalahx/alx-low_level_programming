1191f5c327b43ac88559bc02881d849527da975c
Salah <salah.elhssani@gmail.com>
2023-04-11 04:15:30 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,36 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int l1 = 0;
 	int l2 = 0;
-
-	while (s1[l1])
-		l1++;
-	while (s2[l2])
-		l2++;
+	
+	if (s1)
+	{
+		while (s1[l1])
+			l1++;
+	}
+	if (s2)
+	{
+	
+		while (s2[l2])
+			l2++;
+	}
 	s = (char *) malloc((l1 + l2 + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
-	while (l2 >= 0)
+	if (s2)
 	{
-		s[l1 + l2] = s2[l2];
-		l2--;
+		while (l2 >= 0)
+		{
+			s[l1 + l2] = s2[l2];
+			l2--;
+		}
 	}
-	while (l1 - 1 >= 0)
+	if (s1)
 	{
-		s[l1 - 1] = s1[l1 - 1];
-		l1--;
+		while (l1 - 1 >= 0)
+		{
+			s[l1 - 1] = s1[l1 - 1];
+			l1--;
+		}
 	}
 	return (s);
 }
