9368ec8a09d194adb7ca7bdad027f49bcdc83474
Salah <salah.elhssani@gmail.com>
2023-04-13 06:52:37 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,5 +27,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	for (i = 0; i < (new_size < old_size ? new_size : old_size); i++)
 		*((char *) p + i) = *((char *) ptr + i);
+	free(ptr);
 	return (p);
 }
