ff5483384d46aca89bc473c0c04f44c9f725cbd8
Salah <salah.elhssani@gmail.com>
2023-04-13 18:51:51 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -15,30 +15,25 @@ char *multiply(char *num1, char *num2, int len1, int len2);
  */
 int main(int argc, char *argv[])
 {
-    char *result;
-    int len1, len2;
-    int i = 0;
+	char *result;
+	int len1, len2;
+	int i = 0;
 
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		print_error();
+		exit(98);
+	}
+	len1 = _strlen(argv[1]);
+	len2 = _strlen(argv[2]);
+	result = multiply(argv[1], argv[2], len1, len2);
 
-    if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
-    {
-        print_error();
-        exit(98);
-    }
-
-
-    len1 = _strlen(argv[1]);
-    len2 = _strlen(argv[2]);
-
-    result = multiply(argv[1], argv[2], len1, len2);
-
-    if (result[i] == '0')
-        i++;
-    for (; result[i]; i++)
-            _putchar(result[i]);
-
-    _putchar('\n');
-    return (0);
+	if (result[i] == '0')
+		i++;
+	for (; result[i]; i++)
+		_putchar(result[i]);
+	_putchar('\n');
+	return (0);
 }
 
 /**
@@ -49,10 +44,10 @@ int main(int argc, char *argv[])
  */
 int _isdigit(char *str)
 {
-    for (; *str; str++)
-        if (*str < '0' || *str > '9')
-            return (0);
-    return (1);
+	for (; *str; str++)
+		if (*str < '0' || *str > '9')
+			return (0);
+	return (1);
 }
 
 /**
@@ -63,11 +58,11 @@ int _isdigit(char *str)
  */
 int _strlen(char *str)
 {
-    int len = 0;
+	int len = 0;
 
-    for (; *str; str++)
-        len++;
-    return (len);
+	for (; *str; str++)
+		len++;
+	return (len);
 }
 
 /**
@@ -75,12 +70,12 @@ int _strlen(char *str)
  */
 void print_error(void)
 {
-    _putchar('E');
-    _putchar('r');
-    _putchar('r');
-    _putchar('o');
-    _putchar('r');
-    _putchar('\n');
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
 }
 
 /**
@@ -94,36 +89,32 @@ void print_error(void)
  */
 char *multiply(char *num1, char *num2, int len1, int len2)
 {
-    int i, j;
-    int n1, n2, mul, carry;
-    char *result;
+	int i, j;
+	int n1, n2, mul, carry;
+	char *result;
 
-    result = malloc(len1 + len2 + 1);
-    if (!result)
-    {
-        print_error();
-        exit(98);
-    }
-
-    for (i = 0; i < len1 + len2; i++)
-        result[i] = '0';
-
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        n1 = num1[i] - '0';
-        carry = 0;
-
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            n2 = num2[j] - '0';
-            mul = n1 * n2 + (result[i + j + 1] - '0') + carry;
-            carry = mul / 10;
-            result[i + j + 1] = (mul % 10) + '0';
-        }
-        result[i] += carry;
-    }
-    result[len1 + len2] = '\0';
-
-    return (result);
+	result = malloc(len1 + len2 + 1);
+	if (!result)
+	{
+		print_error();
+		exit(98);
+	}
+	for (i = 0; i < len1 + len2; i++)
+		result[i] = '0';
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			mul = n1 * n2 + (result[i + j + 1] - '0') + carry;
+			carry = mul / 10;
+			result[i + j + 1] = (mul % 10) + '0';
+		}
+		result[i] += carry;
+	}
+	result[len1 + len2] = '\0';
+	return (result);
 }
 
