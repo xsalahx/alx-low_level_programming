0ae18073a2b31646c695191d2b5b3637abd0bc99
Salah <salah.elhssani@gmail.com>
2023-04-14 09:59:02 -0700

    All Tasks

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", (d->name == NULL ? "(nil)" : d->name));
+	if (d->age == 0)
+		printf("Age: %s\n", "(nil)");
+	else
+		printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", (d->owner == NULL ? "(nil)" : d->owner));
+}
---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,60 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ *
+ * Return: lenght of s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *n;
+	char *o;
+	struct dog *d, dog;
+
+	int ln;
+	int lo;
+	int i;
+
+	ln = _strlen_recursion(name);
+	lo = _strlen_recursion(owner);
+
+	n = (char *) malloc(sizeof(char) * (ln + 1));
+	if (n == NULL)
+		return (NULL);
+	o = (char *) malloc(sizeof(char) * (lo + 1));
+	if (o == NULL)
+		return (NULL);
+
+	for (i = 0; i <= ln; i++)
+		n[i] = name[i];	
+	for (i = 0; i <= lo; i++)
+		o[i] = owner[i];
+	dog.name = n;
+	dog.age = age;
+	dog.owner = o;
+	d = (dog_t *) malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	*d = dog;
+	return (d);
+}
