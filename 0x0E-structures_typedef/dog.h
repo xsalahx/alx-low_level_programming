b9fa731d51cc19594d239b2843a0e11454e9c57a
Salah <salah.elhssani@gmail.com>
2023-04-17 04:51:23 -0700

    Task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog;
-typedef struct dog dog_t; 
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -10,7 +9,7 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 /**
- * dog - dog structure
+ * struct dog - dog structure
  * @name: name
  * @age: age
  * @owner: owner
