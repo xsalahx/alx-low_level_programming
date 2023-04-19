7f84d96ade0e3edcd6d0eb214919e419bcca7d26
Salah <salah.elhssani@gmail.com>
2023-04-19 07:40:02 -0700

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv)
 {
 	int bytes = atoi(argv[1]);
 	int i;
-	const unsigned char *f = (unsigned char *) main;
+	unsigned char *f = (unsigned char *) main;
 
 	if (argc != 2 || bytes == 0)
 	{
