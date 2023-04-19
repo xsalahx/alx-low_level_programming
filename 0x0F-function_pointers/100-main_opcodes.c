c383d87abdba00f84e7b0e827b8d267ee63c45b0
Salah <salah.elhssani@gmail.com>
2023-04-19 06:10:48 -0700

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: Exit status
+ */
+int main(int argc, char **argv)
+{
+	int bytes = atoi(argv[1]);
+	int i;
+	const unsigned char *f = (unsigned char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (bytes <= 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%.2x ", f[i]);
+		if (i + 1 == bytes)
+			printf("\n");
+		else
+			printf(" ");
+	}
+	return (0);
+}
