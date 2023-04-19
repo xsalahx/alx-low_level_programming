7049b2be9d8c9b969ef0922eb7b93b0a16ab3d8b
Salah <salah.elhssani@gmail.com>
2023-04-19 04:51:25 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -7,5 +8,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if (f != NULL && name != NULL)
+		(*f)(name);
 }
