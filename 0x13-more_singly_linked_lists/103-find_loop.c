38ca9f8d07a25eca43378aa094482b4fd5a7b6e9
Salah <salah.elhssani@gmail.com>
2023-05-01 12:16:03 -0700

    TAsk 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -3,6 +3,28 @@
 #include <stdlib.h>
 
 /**
+ * check_node - checks if the node exist in a previous index
+ * @head: list head
+ * @node: node
+ * @idx: index
+ *
+ * Return: bool
+ */
+int check_node(const listint_t *head, const listint_t *node, int idx)
+{
+	int size = 0;
+
+	while (size < idx && head != NULL)
+	{
+		if (head == node)
+			return (1);
+		head = head->next;
+		size++;
+	}
+	return (0);
+}
+
+/**
  * find_listint_loop - finds the loop in a linked list.
  * @head: list head
  *
