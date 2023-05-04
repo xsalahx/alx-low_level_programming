2e02329616c177db1146960a61866d9903ea41b8
Salah <salah.elhssani@gmail.com>
2023-05-04 10:15:21 -0700

    Update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: binary number (char *)
+ *
+ * Return: number (unsigned int)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	
+	if (b == NULL)
+		return (0);
+	for (; *b != '\0'; b++)
+	{
+		if (*b != '1' && *b != '0')
+			return (0);
+		else
+		{
+			n = n ^ (unsigned int) (*b - '0');
+			if (*(b + 1) != '\0')
+				n = n << 1;
+		}
+	}
+	return (n);
+}
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to print
+ */
+void print_binary(unsigned long int n)
+{
+	d
+}
