25a8c83c75bb1fcc5392206519a874bfe219a40c
Salah <salah.elhssani@gmail.com>
2023-05-04 10:17:35 -0700

    Fix betty task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,16 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0;
-	
+
 	if (b == NULL)
 		return (0);
 	for (; *b != '\0'; b++)
 	{
 		if (*b != '1' && *b != '0')
 			return (0);
-		else
-		{
-			n = n ^ (unsigned int) (*b - '0');
-			if (*(b + 1) != '\0')
-				n = n << 1;
-		}
+		n = n ^ (unsigned int) (*b - '0');
+		if (*(b + 1) != '\0')
+			n = n << 1;
 	}
 	return (n);
 }
