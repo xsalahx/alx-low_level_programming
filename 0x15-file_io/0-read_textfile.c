267335a85d8cebdab9d82e649b199140557170b4
Salah <salah.elhssani@gmail.com>
2023-05-08 15:00:45 -0700

    fix betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
  * @filename: filename
- * @letter: number of letter to print
+ * @letters: number of letter to print
  *
  * Return: actual number of letters it could read and print
  */
